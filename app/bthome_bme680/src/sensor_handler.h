1a5d2d687089177ba7557609029e46163b092351
Dipak Shetty <shetty.dipak@gmx.com>
2025-06-13 22:34:00 +0200

    app: bthome_bme680: use bme68x_iaq driver
    Now uses the bme68x_iaq driver copied over from nrf sdk.
    
    Signed-off-by: Dipak Shetty <shetty.dipak@gmx.com>

---
+++ app/bthome_bme680/src/sensor_handler.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2025 Dipak Shetty <shetty.dipak@gmx.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef SENSOR_HANDLER_H_
+#define SENSOR_HANDLER_H_
+
+
+
+/**
+ * @brief Initialize the sensor device and work items
+ *
+ * @return 0 on success, negative error code otherwise
+ */
+int init_sensor(void);
+
+#endif /* SENSOR_HANDLER_H_ */
+
